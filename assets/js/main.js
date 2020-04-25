762b45059eaeee274867191f2a531bf383f99801
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-25 19:31:45 +0200

    last update

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+let test=15.16698
+
+
+console.log(test.toString(2))
+console.log(test.toString(4))
+console.log(test.toString(6))
+console.log(test.toString(8))
+
+
+let test2=7.78714 
+console.log(test2.toString(2))
+console.log(test2.toString(4))
+console.log(test2.toString(6))
+console.log(test2.toString(8))
